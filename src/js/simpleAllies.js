6b3eaa22dc520f37d66c1472e41f7b3664e840eb
Robalian <robalian94@gmail.com>
2023-12-15 12:37:08 +0100

    formatting

--- src/js/simpleAllies.js
+++ src/js/simpleAllies.js
@@ -72,86 +72,84 @@ class SimpleAllies {
     }
 
     // Request methods
-	/**
-	 * Request resource
-	 * @param {Object} args - a request object
-	 * @param {number} args.priority - 0-1 where 1 is highest consideration
-	 * @param {string} args.roomName
-	 * @param {ResourceConstant} args.resourceType
-	 * @param {number} args.amount - How much they want of the resource. If the responder sends only a portion of what you ask for, that's fine
-	 * @param {boolean} [terminal] - If the bot has no terminal, allies should instead haul the resources to us
-	 */
+    /**
+     * Request resource
+     * @param {Object} args - a request object
+     * @param {number} args.priority - 0-1 where 1 is highest consideration
+     * @param {string} args.roomName
+     * @param {ResourceConstant} args.resourceType
+     * @param {number} args.amount - How much they want of the resource. If the responder sends only a portion of what you ask for, that's fine
+     * @param {boolean} [terminal] - If the bot has no terminal, allies should instead haul the resources to us
+     */
     requestResource(args) {
         this.myRequests.resource.push(args);
     }
-	
-	/**
-	 * Request help in defending a room
-	 * @param {Object} args - a request object
-	 * @param {number} args.priority - 0-1 where 1 is highest consideration
-	 * @param {string} args.roomName
-	 */
+    
+    /**
+     * Request help in defending a room
+     * @param {Object} args - a request object
+     * @param {number} args.priority - 0-1 where 1 is highest consideration
+     * @param {string} args.roomName
+     */
     requestDefense(args) {
         this.myRequests.defense.push(args);
     }
-	
-	/**
-	 * Request an attack on a specific room
-	 * @param {Object} args - a request object
-	 * @param {number} args.priority - 0-1 where 1 is highest consideration
-	 * @param {string} args.roomName
-	 */
+    
+    /**
+     * Request an attack on a specific room
+     * @param {Object} args - a request object
+     * @param {number} args.priority - 0-1 where 1 is highest consideration
+     * @param {string} args.roomName
+     */
     requestAttack(args) {
         this.myRequests.attack.push(args);
     }
-	
-	/**
-	 * Influence allies aggresion score towards a player
-	 * @param {Object} args - a request object
-	 * @param {number} args.hate - 0-1 where 1 is highest consideration. How much you think your team should hate the player. Should probably affect combat aggression and targetting
-	 * @param {number} args.lastAttackedBy - The last time this player has attacked you
-	 */
+    
+    /**
+     * Influence allies aggresion score towards a player
+     * @param {Object} args - a request object
+     * @param {number} args.hate - 0-1 where 1 is highest consideration. How much you think your team should hate the player. Should probably affect combat aggression and targetting
+     * @param {number} args.lastAttackedBy - The last time this player has attacked you
+     */
     requestPlayer(args) {
         this.myRequests.player.push(args);
     }
-	
-	/**
-	 * Request help in building/fortifying a room
-	 * @param {Object} args - a request object
-	 * @param {string} args.roomName
-	 * @param {number} args.priority - 0-1 where 1 is highest consideration
-	 * @param {'build' | 'repair'} args.workType
-	 */
+    
+    /**
+     * Request help in building/fortifying a room
+     * @param {Object} args - a request object
+     * @param {string} args.roomName
+     * @param {number} args.priority - 0-1 where 1 is highest consideration
+     * @param {'build' | 'repair'} args.workType
+     */
     requestWork(args) {
         this.myRequests.work.push(args);
     }
-	
-	/**
-	 * Share how your bot is doing economically
-	 * @param {Object} args - a request object
-	 * @param {number} args.credits - total credits the bot has. Should be 0 if there is no market on the server
-	 * @param {number} args.sharableEnergy - the maximum amount of energy the bot is willing to share with allies. Should never be more than the amount of energy the bot has in storing structures
-	 * @param {number} [args.energyIncome] - The average energy income the bot has calculated over the last 100 ticks
-     * Optional, as some bots might not be able to calculate this easily.
-	 * @param {Object.<MineralConstant, number>} [args.mineralNodes] - The number of mineral nodes the bot has access to, probably used to inform expansion
-	 */
+    
+    /**
+     * Share how your bot is doing economically
+     * @param {Object} args - a request object
+     * @param {number} args.credits - total credits the bot has. Should be 0 if there is no market on the server
+     * @param {number} args.sharableEnergy - the maximum amount of energy the bot is willing to share with allies. Should never be more than the amount of energy the bot has in storing structures
+     * @param {number} [args.energyIncome] - The average energy income the bot has calculated over the last 100 ticks. Optional, as some bots might not be able to calculate this easily.
+     * @param {Object.<MineralConstant, number>} [args.mineralNodes] - The number of mineral nodes the bot has access to, probably used to inform expansion
+     */
     requestEcon(args) {
         this.myRequests.econ = args;
     }
-	
-	
-	/**
-	 * Share scouting data about hostile owned rooms
-	 * @param {Object} args - a request object
-	 * @param {string} args.roomName
-	 * @param {string} args.playerName - The player who owns this room. If there is no owner, the room probably isn't worth making a request about
-	 * @param {number} args.lastScout - The last tick your scouted this room to acquire the data you are now sharing
-	 * @param {number} args.rcl
-	 * @param {number} args.energy - The amount of stored energy the room has. storage + terminal + factory should be sufficient
-	 * @param {number} args.towers
-	 * @param {number} args.avgRamprtHits
-	 * @param {boolean} args.terminal - has terminal built
-	 */
+    
+    /**
+     * Share scouting data about hostile owned rooms
+     * @param {Object} args - a request object
+     * @param {string} args.roomName
+     * @param {string} args.playerName - The player who owns this room. If there is no owner, the room probably isn't worth making a request about
+     * @param {number} args.lastScout - The last tick your scouted this room to acquire the data you are now sharing
+     * @param {number} args.rcl
+     * @param {number} args.energy - The amount of stored energy the room has. storage + terminal + factory should be sufficient
+     * @param {number} args.towers
+     * @param {number} args.avgRamprtHits
+     * @param {boolean} args.terminal - does scouted room have terminal built
+     */
     requestRoom(args) {
         this.myRequests.room.push(args);
     }
